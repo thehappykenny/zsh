8c63740fd2abac6e7e4bb2344e96fc72eb9f4c99
wp <859994178@qq.com>
2024-08-10 16:56:28 +0800

    ignore

--- proxy_git.zsh
+++ proxy_git.zsh
@@ -1,1 +1,17 @@
-/home/wp/.dotfiles/tag-bin/bin/tools/proxy_git.zsh
+#!/bin/zsh
+
+function proxy_on() {
+	export http_proxy="http://127.0.0.1:38457"
+	export https_proxy=$http_proxy
+	echo -e "终端代理开启。"
+}
+
+function proxy_off() {
+	unset http_proxy https_proxy
+	echo -e "终端代理关闭。"
+}
+
+function proxy_status() {
+	env|grep -i proxy
+	echo -e "终端代理状态。"
+}
